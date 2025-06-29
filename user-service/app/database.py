f58e7bf3ede8686a5fef3f208cf3b5f674170f64
vejat <volea19@alumnos.utalca.cl>
2025-06-29 03:48:51 -0400

    asa

--- user-service/app/database.py
+++ user-service/app/database.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base   
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-# Carga el .env del user-service
 load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME", "users.db")
-DATABASE_URL = f"sqlite:///./{DB_NAME}"
 
+# Detecta si es ruta absoluta
+if DB_NAME.startswith("/"):
+    DATABASE_URL = f"sqlite:///{DB_NAME}"
+else:
+    DATABASE_URL = f"sqlite:///./{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
